a4d6694e3fcdc3b698a37531c2dea8274f0eef17
beruseruko <mrcoolfoxythegamer@mail.ru>
2025-11-23 15:11:07 +0700

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react(), markdoc(), keystatic()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [react(), markdoc()],
 });
